58b4dedcc08719f05336ec1a789db2b6eda3d794
Ewan Barr <ewan.d.barr@googlemail.com>
2020-05-11 21:32:09 +0200

    first pass at sqla based uploader

---
+++ trapum_db.py
@@ -0,0 +1,261 @@
+# coding: utf-8
+from sqlalchemy import Column, DateTime, Float, ForeignKey, Index, String, Table, Text
+from sqlalchemy.dialects.mysql import INTEGER, TINYINT
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Affiliation(Base):
+    __tablename__ = 'affiliation'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(64), unique=True)
+    fullname = Column(String(128), unique=True)
+    address = Column(String(256))
+
+    users = relationship('User', secondary='affiliations')
+
+
+class BeamformerConfiguration(Base):
+    __tablename__ = 'beamformer_configuration'
+
+    id = Column(INTEGER(11), primary_key=True)
+    centre_frequency = Column(Float)
+    bandwidth = Column(Float)
+    incoherent_nchans = Column(INTEGER(11))
+    incoherent_tsamp = Column(Float)
+    incoherent_antennas = Column(Text)
+    coherent_nchans = Column(INTEGER(11))
+    coherent_tsamp = Column(Float)
+    coherent_antennas = Column(Text)
+    configuration_authority = Column(String(64))
+    receiver = Column(String(20))
+    metainfo = Column(Text)
+
+
+class FileAction(Base):
+    __tablename__ = 'file_action'
+
+    id = Column(INTEGER(11), primary_key=True)
+    action = Column(String(64), nullable=False)
+    is_destructive = Column(TINYINT(1), nullable=False)
+
+
+class FileType(Base):
+    __tablename__ = 'file_type'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(64), nullable=False, unique=True)
+    description = Column(Text)
+
+
+class Hardware(Base):
+    __tablename__ = 'hardware'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(Text)
+    metainfo = Column(Text)
+    notes = Column(Text)
+
+
+class MembershipRole(Base):
+    __tablename__ = 'membership_role'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(64), unique=True)
+
+    users = relationship('User', secondary='membership_roles')
+
+
+class Pipeline(Base):
+    __tablename__ = 'pipeline'
+
+    id = Column(INTEGER(11), primary_key=True)
+    hash = Column(String(100), unique=True)
+    name = Column(String(64), nullable=False, unique=True)
+    arguments_json = Column(Text)
+    notes = Column(Text)
+
+
+class ProcessingArgument(Base):
+    __tablename__ = 'processing_arguments'
+
+    id = Column(INTEGER(11), primary_key=True)
+    arguments_json = Column(Text)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(INTEGER(11), primary_key=True)
+    username = Column(String(64), unique=True)
+    fullname = Column(String(64))
+    email = Column(String(120), unique=True)
+    password_hash = Column(String(128))
+    administrator = Column(TINYINT(1))
+    token = Column(String(32), unique=True)
+    token_expiration = Column(DateTime)
+
+    working_groups = relationship('WorkingGroup', secondary='memberships')
+
+
+t_affiliations = Table(
+    'affiliations', metadata,
+    Column('user_id', ForeignKey('user.id'), primary_key=True, nullable=False),
+    Column('affiliation_id', ForeignKey('affiliation.id'), primary_key=True, nullable=False, index=True)
+)
+
+
+t_membership_roles = Table(
+    'membership_roles', metadata,
+    Column('user_id', ForeignKey('user.id'), primary_key=True, nullable=False),
+    Column('membership_role_id', ForeignKey('membership_role.id'), primary_key=True, nullable=False, index=True)
+)
+
+
+class Processing(Base):
+    __tablename__ = 'processing'
+
+    id = Column(INTEGER(11), primary_key=True)
+    pipeline_id = Column(ForeignKey('pipeline.id', onupdate='CASCADE'), nullable=False, index=True)
+    hardware_id = Column(ForeignKey('hardware.id', onupdate='CASCADE'), index=True)
+    arguments_id = Column(ForeignKey('processing_arguments.id', onupdate='CASCADE'), index=True)
+    submit_time = Column(DateTime)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    process_status = Column(String(20))
+
+    arguments = relationship('ProcessingArgument')
+    hardware = relationship('Hardware')
+    pipeline = relationship('Pipeline')
+
+
+class WorkingGroup(Base):
+    __tablename__ = 'working_group'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(120), unique=True)
+    description = Column(String(500))
+    chair_user_id = Column(ForeignKey('user.id', onupdate='CASCADE'), index=True)
+    outreach_liaison_user_id = Column(ForeignKey('user.id', onupdate='CASCADE'), index=True)
+
+    chair_user = relationship('User', primaryjoin='WorkingGroup.chair_user_id == User.id')
+    outreach_liaison_user = relationship('User', primaryjoin='WorkingGroup.outreach_liaison_user_id == User.id')
+
+
+t_memberships = Table(
+    'memberships', metadata,
+    Column('user_id', ForeignKey('user.id'), primary_key=True, nullable=False),
+    Column('working_group_id', ForeignKey('working_group.id'), primary_key=True, nullable=False, index=True)
+)
+
+
+class Project(Base):
+    __tablename__ = 'project'
+
+    id = Column(INTEGER(11), primary_key=True)
+    name = Column(String(120), unique=True)
+    description = Column(String(500))
+    working_group_id = Column(ForeignKey('working_group.id', onupdate='CASCADE'), index=True)
+    coordinator_user_id = Column(ForeignKey('user.id', onupdate='CASCADE'), index=True)
+
+    coordinator_user = relationship('User')
+    working_group = relationship('WorkingGroup')
+
+
+class Target(Base):
+    __tablename__ = 'target'
+
+    id = Column(INTEGER(11), primary_key=True)
+    project_id = Column(ForeignKey('project.id', onupdate='CASCADE'), index=True)
+    source_name = Column(String(64))
+    ra = Column(String(20))
+    dec = Column(String(20))
+    region = Column(String(20))
+    semi_major_axis = Column(Float)
+    semi_minor_axis = Column(Float)
+    position_angle = Column(Float)
+    metainfo = Column(Text)
+    notes = Column(Text)
+
+    project = relationship('Project')
+
+
+class Pointing(Base):
+    __tablename__ = 'pointing'
+
+    id = Column(INTEGER(11), primary_key=True)
+    target_id = Column(ForeignKey('target.id', onupdate='CASCADE'), nullable=False, index=True)
+    bf_config_id = Column(ForeignKey('beamformer_configuration.id', onupdate='CASCADE'), nullable=False, index=True)
+    observation_length = Column(Float)
+    utc_start = Column(DateTime, nullable=False)
+    sb_id = Column(Text, nullable=False)
+    mkat_pid = Column(Text, nullable=False)
+    beam_shape = Column(Text)
+
+    bf_config = relationship('BeamformerConfiguration')
+    target = relationship('Target')
+
+
+class Beam(Base):
+    __tablename__ = 'beam'
+
+    id = Column(INTEGER(11), primary_key=True)
+    pointing_id = Column(ForeignKey('pointing.id', onupdate='CASCADE'), nullable=False, index=True)
+    on_target = Column(TINYINT(1), nullable=False)
+    ra = Column(String(20))
+    dec = Column(String(20))
+    coherent = Column(TINYINT(1))
+    name = Column(String(20))
+
+    pointing = relationship('Pointing')
+
+
+class DataProduct(Base):
+    __tablename__ = 'data_product'
+    __table_args__ = (
+        Index('_full_file_path', 'filename', 'filepath', unique=True),
+    )
+
+    id = Column(INTEGER(11), primary_key=True, index=True)
+    pointing_id = Column(ForeignKey('pointing.id', onupdate='CASCADE'), nullable=False, index=True)
+    beam_id = Column(ForeignKey('beam.id', onupdate='CASCADE'), nullable=False, index=True)
+    processing_id = Column(ForeignKey('processing.id', onupdate='CASCADE'), index=True)
+    file_type_id = Column(ForeignKey('file_type.id', onupdate='CASCADE'), nullable=False, index=True)
+    filename = Column(String(64), nullable=False)
+    filepath = Column(String(255), nullable=False)
+    filehash = Column(String(100))
+    available = Column(TINYINT(1), nullable=False)
+    locked = Column(TINYINT(1), nullable=False)
+    upload_date = Column(DateTime, nullable=False)
+    modification_date = Column(DateTime, nullable=False)
+
+    beam = relationship('Beam')
+    file_type = relationship('FileType')
+    pointing = relationship('Pointing')
+    processing = relationship('Processing')
+    processings = relationship('Processing', secondary='processing_inputs')
+
+
+class FileActionRequest(Base):
+    __tablename__ = 'file_action_request'
+
+    id = Column(INTEGER(11), primary_key=True)
+    data_product_id = Column(ForeignKey('data_product.id', onupdate='CASCADE'), nullable=False, index=True)
+    action_id = Column(ForeignKey('file_action.id', onupdate='CASCADE'), nullable=False, index=True)
+    requested_at = Column(DateTime, nullable=False)
+    completed_at = Column(DateTime)
+    success = Column(TINYINT(1), nullable=False)
+
+    action = relationship('FileAction')
+    data_product = relationship('DataProduct')
+
+
+t_processing_inputs = Table(
+    'processing_inputs', metadata,
+    Column('dp_id', ForeignKey('data_product.id'), primary_key=True, nullable=False),
+    Column('processing_id', ForeignKey('processing.id'), primary_key=True, nullable=False, index=True)
+)
---
+++ upload.py
@@ -0,0 +1,346 @@
+import json
+import glob
+import os
+import logging
+import xxhash
+import optparse
+import time
+import datetime
+from functools import wraps
+from contextlib import contextmanager
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+from trapum_db import (
+    BeamformerConfig, Target, Pointing,
+    Beam, DataProduct, FileType)
+from header_util import parseSigprocHeader, updateHeader
+
+log = logging.getLogger('trapum_db.upload')
+
+
+def parse_katpoint_string(kptarget):
+    names, _, ra, dec = kptarget.split(",")
+    primary_name = names.split("|")[0]
+    return primary_name.strip(), ra.strip(), dec.strip()
+
+
+def read_apsuse_metafile(filename):
+    """
+    @ brief Retrieve info from meta file in path
+
+    @parans File path of interest
+
+    @return all APSUSE meta info as a dictionary
+    """
+    with open(filename, 'r', encoding='utf-8') as json_file:
+        am_config_info = json.load(json_file)
+    return am_config_info
+
+
+class NoMetaFileException(Exception):
+    pass
+
+
+class TimeTracker(object):
+    def __init__(self):
+        self.count = 0
+        self.elapsed = 0.0
+
+    def add(self, duration):
+        self.count += 1
+        self.elapsed += duration
+
+
+class Timer(object):
+    trackers = {}
+
+    @staticmethod
+    def track(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            start = time.time()
+            retval = func(*args, **kwargs)
+            duration = time.time() - start
+            name = func.__name__
+            if name in Timer.trackers.keys():
+                Timer.trackers[name].add(duration)
+            else:
+                t = TimeTracker()
+                t.add(duration)
+                Timer.trackers[name] = t
+            return retval
+        return wrapped
+
+    @staticmethod
+    def summary():
+        for func, tracker in Timer.trackers.items():
+            print("Func: {}, Called: {}, Total: {} s, Avg: {} s".format(
+                func, tracker.count, tracker.elapsed,
+                tracker.elapsed/tracker.count))
+
+
+class TrapumUploader(object):
+    def __init__(self, database):
+        self._session_engine = create_engine(database, echo=False)
+        self._session_factory = sessionmaker(
+            bind=self._session_engine, poolclass=NullPool)
+
+    @contextmanager
+    def session(self):
+        session = self._session_factory()
+        try:
+            yield session
+            session.commit()
+        except Exception as error:
+            session.rollback()
+            raise error
+        finally:
+            session.close()
+
+    @Timer.track
+    def _get_bf_config_id(self, metadata):
+        bf_params = dict(
+                centre_frequency=metadata['centre_frequency'],
+                bandwidth=metadata['bandwidth'],
+                incoherent_nchans=metadata['incoherent_nchans'],
+                incoherent_tsamp=metadata['incoherent_tsamp'],
+                incoherent_antennas=metadata.get('incoherent_antennas', ''),
+                coherent_nchans=metadata['coherent_nchans'],
+                coherent_tsamp=metadata['coherent_tsamp'],
+                coherent_antennas=metadata.get('coherent_antennas', ''),
+                configuration_authority=metadata.get('configuration_authority', ''),
+                receiver=metadata.get('receiver', ''),
+                metainfo=metadata.get('metadata', '')
+            )
+        with self.session() as session:
+            bf_config_id = session.query(
+                    BeamformerConfig.id
+                ).filter(
+                    BeamformerConfig.centre_frequency == bf_params['centre_frequency'],
+                    BeamformerConfig.bandwidth == bf_params['bandwidth'],
+                    BeamformerConfig.incoherent_nchans == bf_params['incoherent_nchans'],
+                    BeamformerConfig.incoherent_tsamp == bf_params['incoherent_tsamp'],
+                    BeamformerConfig.incoherent_antennas.ilike(bf_params['incoherent_antennas']),
+                    BeamformerConfig.coherent_nchans == bf_params['coherent_nchans'],
+                    BeamformerConfig.coherent_tsamp == bf_params['coherent_tsamp'],
+                    BeamformerConfig.coherent_antennas.ilike(bf_params['coherent_antennas']),
+                    BeamformerConfig.configuration_authority.ilike(bf_params['configuration_authority']),
+                    BeamformerConfig.receiver.ilike(bf_params['receiver']),
+                    BeamformerConfig.metainfo.ilike(bf_params['metadata'])
+                ).scalar()
+
+            if not bf_config_id:
+                bf_config = BeamformerConfig(**bf_params)
+                session.add(bf_config)
+                session.flush()
+                bf_config_id = bf_config.id
+        return bf_config_id
+
+    @Timer.track
+    def _get_target_id(self, kptarget):
+        name, ra, dec = parse_katpoint_string(kptarget)
+        target_params = dict(
+            source_name=name,
+            ra=ra,
+            dec=dec)
+        with self.session() as session:
+            target_id = session.query(
+                    Target.id
+                ).filter(
+                    Target.source_name.ilike(target_params['source_name']),
+                ).scalar()
+            if not target_id:
+                target = Target(**target_params)
+                session.add(target)
+                session.flush()
+                target_id = target.id
+        return target_id
+
+    @Timer.track
+    def _get_pointing_id(self, target_id, bf_config_id, metadata):
+        utc_start = datetime.datetime.strptime(metadata['utc_start'], '%Y/%m/%d %H:%M:%S')
+        pointing_params = dict(
+            target_id=target_id,
+            bf_config_id=bf_config_id,
+            utc_start=utc_start,
+            sb_id=metadata['sb_id']
+            )
+        with self.session() as session:
+            pointing_id = session.query(
+                    Pointing.id
+                ).filter(
+                    Pointing.target_id == target_id,
+                    Pointing.bf_config_id == bf_config_id,
+                    Pointing.utc_start == utc_start,
+                    Pointing.sb_id.like(metadata['sb_id']),
+                    Pointing.mkat_pid.like(metadata['project_name'])
+                ).scalar()
+            if not pointing_id:
+                pointing = Pointing(**pointing_params)
+                session.add(pointing)
+                session.flush()
+                pointing_id = pointing.id
+        return pointing_id
+
+    @Timer.track
+    def _get_beam_id(self, kptarget, beamname, pointing_id):
+        _, ra, dec = parse_katpoint_string(kptarget)
+        with self.session() as session:
+            beam_id = session.query(
+                    Beam.id
+                ).filter(
+                    Beam.pointing_id == pointing_id,
+                    Beam.name.like(beamname)
+                ).scalar()
+            if not beam_id:
+                beam = Beam(
+                    pointing_id=pointing_id,
+                    on_target=True,
+                    ra=ra,
+                    dec=dec,
+                    coherent="cfbf" in beamname,
+                    name=beamname
+                    )
+                session.add(beam)
+                session.flush()
+                beam_id = beam.id
+        return beam_id
+
+    @Timer.track
+    def _get_file_type_id(self, name):
+        with self.session() as session:
+            ft_id = session.query(
+                    FileType.id
+                ).filter_by(
+                    name=name
+                ).scalar()
+            if not ft_id:
+                ft = FileType(
+                    name=name
+                    )
+                session.add(ft)
+                session.flush()
+                ft_id = ft.id
+        return ft_id
+
+    @Timer.track
+    def _generate_filehash(self, filepath):
+        xx = xxhash.xxh64()
+        with open(filepath, 'rb') as f:
+            xx.update(f.read(10000))
+            xx.update(b'{}'.format(os.path.getsize(filepath)))
+        return xx.hexdigest()
+
+    @Timer.track
+    def _get_dp_id(self, pointing_id, beam_id, file_type_id, filepath, filename):
+        with self.session() as session:
+            dp_id = session.query(
+                    DataProduct.id
+                ).filter(
+                    DataProduct.pointing_id == pointing_id,
+                    DataProduct.beam_id == beam_id,
+                    DataProduct.file_type_id == file_type_id,
+                    DataProduct.filename.like(filename),
+                    DataProduct.filepath.like(filepath)
+                ).scalar()
+            if not dp_id:
+                dp = DataProduct(
+                    pointing_id=pointing_id,
+                    beam_id=beam_id,
+                    file_type_id=file_type_id,
+                    processing_id=None,
+                    filename=filename,
+                    filepath=filepath,
+                    filehash=self._generate_filehash(
+                        os.path.join(filepath, filename)),
+                    available=True,
+                    locked=True
+                    )
+                session.add(dp)
+                session.flush()
+                dp_id = dp.id
+        return dp_id
+
+    @Timer.track
+    def _validate_header(self, filterbank):
+        try:
+            header = parseSigprocHeader(filterbank)
+            header = updateHeader(header)
+        except:
+            return False
+        else:
+            return True
+
+    @Timer.track
+    def scrape_directory(self, path):
+        metafile = os.path.join(path, 'apsuse.meta')
+        if not os.path.isfile(metafile):
+            log.error('No meta file found in {}'.format(path))
+            raise NoMetaFileException
+        metadata = read_apsuse_metafile(metafile)
+
+        # Note the beam list is a complete dump of all possible beams
+        # not of the beams that are actually recorded
+        beams = sorted(list(metadata['beams'].keys()))
+        valid_beams = [beam for beam in beams if os.path.isdir(
+            os.path.join(path, beam))]
+        bf_config_id = self._get_bf_config_id(metadata)
+
+        target_ids = {}
+        pointing_ids = {}
+
+        cb_filetype = "filterbank-raw-{}-{}us-0dm".format(
+            metadata['coherent_nchans'],
+            int(metadata['coherent_tsamp']*1e6))
+        cb_filetype_id = self._get_file_type_id(cb_filetype)
+
+        ib_filetype = "filterbank-raw-{}-{}us-0dm".format(
+            metadata['incoherent_nchans'],
+            int(metadata['incoherent_tsamp']*1e6))
+        ib_filetype_id = self._get_file_type_id(ib_filetype)
+
+        for beamname in valid_beams:
+            filepath = os.path.join(path, beamname)
+            target_str = metadata['beams'][beamname]
+            if target_str in target_ids.keys():
+                target_id = target_ids[target_str]
+            else:
+                target_id = self._get_target_id(target_str)
+                target_ids[target_str] = target_id
+
+            if target_str in pointing_ids.keys():
+                pointing_id = pointing_ids[target_str]
+            else:
+                pointing_id = self._get_pointing_id(
+                    target_id, bf_config_id, metadata)
+                pointing_ids[target_str] = pointing_id
+            beam_id = self._get_beam_id(target_str, beamname, pointing_id)
+            coherent = "cfbf" in beamname
+            if coherent:
+                file_type_id = cb_filetype_id
+            else:
+                file_type_id = ib_filetype_id
+
+            filterbanks = glob.glob('{}/*.fil'.format(filepath))
+
+            for filterbank in filterbanks:
+                filename = os.path.basename(filterbank)
+                if not self._validate_header(filterbank):
+                    continue
+                dp_id = self._get_dp_id(
+                    pointing_id, beam_id, file_type_id,
+                    filepath, filename)
+
+
+if __name__ == "__main__":
+    FORMAT = "[%(levelname)s - %(asctime)s - %(filename)s:%(lineno)s] %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+    parser = optparse.OptionParser()
+    parser.add_option('--pd', type=str, help="parent directory path", dest="path")
+    parser.add_option('--db', type=str, help="SQLA DB connection string", dest="db")
+    parser.add_option('--log_level', type=str, help="Logging level", dest="log", default="info")
+    opts, args = parser.parse_args()
+    log.setLevel(opts.log)
+    uploader = TrapumUploader(opts.db)
+    uploader.scrape_directory(opts.pd)
